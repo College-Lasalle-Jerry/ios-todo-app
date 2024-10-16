b9869ee741a06b237933c78617fe7a8f9e3d7236
centinna-l <jerryjoy99@gmail.com>
2024-10-16 11:38:30 -0400

    Initial Commit

---
+++ Todo App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by Jerry Joy on 2024-10-16.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
